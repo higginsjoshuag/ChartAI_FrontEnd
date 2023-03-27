bfda4239b114f1206f1968d8658ad27c139372e4
Josh Higgins <higginsjoshuag@gmail.com>
2023-03-27 14:39:59 -0600

    Initial Commit

---
+++ ChartAI/ChartAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartAIApp.swift
+//  ChartAI
+//
+//  Created by Josh Higgins on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ChartAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
